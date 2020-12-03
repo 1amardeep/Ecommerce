50e48de2b6a0a07948745c60f332902f867a945c
1amardeep <amardeepsinghgahlot@gmail.com>
2020-12-03 21:50:46 +0530

    slider

---
+++ src/app/components/angular-material/slider/slider.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic slider
+ */
+@Component({
+  selector: 'slider-overview-example',
+  templateUrl: 'slider.component.html',
+  styleUrls: ['slider.component.scss'],
+})
+export class SliderOverviewExample {}
