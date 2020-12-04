e7e1e1b0a58710ec9dd87b84aa9d8b7bcda217a6
1amardeep <amardeepsinghgahlot@gmail.com>
2020-12-04 20:19:43 +0530

    added files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { MatSliderModule  } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemoMaterialModule } from './material-module';
-import { SliderOverviewExample } from './components/angular-material/slider/slider.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    SliderOverviewExample
+    DashboardComponent    
   ],
   imports: [
     BrowserModule,
@@ -22,7 +20,6 @@ import { SliderOverviewExample } from './components/angular-material/slider/slid
     BrowserAnimationsModule,
     DemoMaterialModule
   ],
-  entryComponents: [SliderOverviewExample],
   providers: [],
   bootstrap: [AppComponent]
 })
