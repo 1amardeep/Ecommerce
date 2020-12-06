b367521a11d5c3381703796f53eac52df07cb1d0
1amardeep <amardeepsinghgahlot@gmail.com>
2020-12-06 18:31:45 +0530

    chart and reactive form added

--- src/app/modules/teams/teams.component.ts
+++ src/app/modules/teams/teams.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { ChartType } from 'chart.js';
-import { MultiDataSet, Label } from 'ng2-charts';
+import { Component, ViewChild } from '@angular/core';
+import { ChartOptions, ChartType } from 'chart.js';
+import { MultiDataSet, Label, BaseChartDirective } from 'ng2-charts';
 import { BackButtonService } from 'src/app/services/back-button.service';
+import { ClientDataService } from 'src/app/services/client-data.service';
 
 @Component({
   selector: 'app-doughnut-chart',
@@ -9,20 +10,47 @@ import { BackButtonService } from 'src/app/services/back-button.service';
   styleUrls: ['./teams.component.scss']
 })
 
+
 export class TeamsComponent {
 
-  doughnutChartLabels: Label[] = ['BMW', 'Ford', 'Tesla', 'Aishwarya'];
-  doughnutChartData: MultiDataSet = [
-    [55, 25, 20, 26]
-  ];
+  @ViewChild(BaseChartDirective, {
+     static: false
+}) chart: BaseChartDirective;
+  
+  doughnutChartLabels: Label[] = [];
+  doughnutChartData: MultiDataSet =[]
+  colorsList: any = [];
+
   doughnutChartType: ChartType = 'doughnut';
 
-  constructor(private backCheck : BackButtonService){
+  constructor(private backCheck : BackButtonService,
+    private clientDataServie : ClientDataService){
 
   }
 
   ngOnInit(): void {
     this.backCheck.backCheck(true);
+    this.clientDataServie.getTableData().subscribe((obj : any) =>{
+      const users =obj.TEAM;
+      this.doughnutChartLabels = users.map(obj => obj.name);
+      this.doughnutChartData= [
+        users.map(obj => obj.win)
+      ];
+      this.colorsList = [
+        {
+          borderColor: 'black',
+          backgroundColor: users.map(obj => obj.color)
+        }
+      ];
+    })
+  }
+
+  public chartOptions: ChartOptions = {
+    legend: { 
+      onClick: (e, i) => {
+        console.log(this.chart.data[0][i.index]);
+      }
+    }
   }
 
 }
